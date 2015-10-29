2cf476e79e68e54f585cf4d9f32040b9e04b709d
Tianon Gravi <tianon@debian.org>
2015-10-29 15:36:56 -0700

    Fix a few odd edge cases in repack.sh

--- debian/repack.sh
+++ debian/repack.sh
@@ -3,27 +3,31 @@
 
 set -e
 
-if ! [ -d debian/repack/prune ]; then
+debDir="$(pwd)/debian"
+
+if [ ! -d "$debDir/repack/prune" ]; then
 	exit 0
 fi
 
-if [ "x$1" != x--upstream-version ]; then
+if [ '--upstream-version' != "$1" ]; then
 	exit 1
 fi
 
 version="$2"
 filename="$3"
 
-if [ -z "$version" ] || ! [ -f "$filename" ]; then
+if [ -z "$version" ] || [ ! -f "$filename" ]; then
 	exit 1
 fi
 
-dir="$(pwd)"
+dir="$(dirname "$filename")"
+filename="$(basename "$filename")"
+dir="$(readlink -f "$dir")"
 tempdir="$(mktemp -d)"
 
 cd "$tempdir"
 tar xf "$dir/$filename"
-cat "$dir"/debian/repack/prune/* | while read file; do
+cat "$debDir"/repack/prune/* | while read file; do
 	if [ -e */"$file" ]; then
 		echo "Pruning $file"
 		rm -rf */"$file"
@@ -31,9 +35,9 @@ cat "$dir"/debian/repack/prune/* | while read file; do
 done
 
 dfsgfilename="$filename"
-if [[ "$dfsgfilename" != *ds* ]]; then
-	pkg="$(dpkg-parsechangelog -l"$dir"/debian/changelog -SSource)"
-	ver="$(dpkg-parsechangelog -l"$dir"/debian/changelog -SVersion)"
+if [[ "$dfsgfilename" != *[~+]ds* ]]; then
+	pkg="$(dpkg-parsechangelog -l"$debDir/changelog" -SSource)"
+	ver="$(dpkg-parsechangelog -l"$debDir/changelog" -SVersion)"
 	origVer="${ver%-*}" # strip everything from the last dash
 	origVer="$(echo "$origVer" | sed -r 's/^[0-9]+://')" # strip epoch
 	upstreamVer="${origVer%%[+~]ds*}"
@@ -41,7 +45,7 @@ if [[ "$dfsgfilename" != *ds* ]]; then
 	
 	dfsgfilename="${dfsgfilename/.orig/$dfsgBits.orig}"
 fi
-tar -czf ${dir}/${dfsgfilename} *
+tar -czf "$dir/$dfsgfilename" *
 cd "$dir"
 rm -rf "$tempdir"
 echo "Done pruning upstream tarball into $dfsgfilename"
