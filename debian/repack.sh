71d9b0d41b009dff270f75ee8fae8177cf0b4a65
Tianon Gravi <tianon@debian.org>
2016-03-01 15:57:09 -0800

    Adjust "repack" script to only repack if we have "DFSG bits" in our version number

--- debian/repack.sh
+++ debian/repack.sh
@@ -20,6 +20,17 @@ if [ -z "$version" ] || [ ! -f "$filename" ]; then
 	exit 1
 fi
 
+debVer="$(dpkg-parsechangelog -l"$debDir/changelog" -SVersion)"
+origVer="${debVer%-*}" # strip everything from the last dash
+origVer="$(echo "$origVer" | sed -r 's/^[0-9]+://')" # strip epoch
+upstreamVer="${origVer%%[+~]ds*}"
+dfsgBits="${origVer#$upstreamVer}"
+
+if [ -z "$dfsgBits" ]; then
+	echo "No 'DFSG' bits in '$debVer' (~ds1 or similar), not pruning"
+	exit 0
+fi
+
 dir="$(dirname "$filename")"
 filename="$(basename "$filename")"
 dir="$(readlink -f "$dir")"
@@ -34,20 +45,13 @@ cat "$debDir"/repack/prune/* | while read file; do
 	fi
 done
 
-dfsgfilename="$filename"
-if [[ "$dfsgfilename" != *[~+]ds* ]]; then
-	pkg="$(dpkg-parsechangelog -l"$debDir/changelog" -SSource)"
-	ver="$(dpkg-parsechangelog -l"$debDir/changelog" -SVersion)"
-	origVer="${ver%-*}" # strip everything from the last dash
-	origVer="$(echo "$origVer" | sed -r 's/^[0-9]+://')" # strip epoch
-	upstreamVer="${origVer%%[+~]ds*}"
-	dfsgBits="${origVer#$upstreamVer}"
-	
-	dfsgfilename="${dfsgfilename/.orig/$dfsgBits.orig}"
+dfsgFilename="$filename"
+if [[ "$dfsgFilename" != *[~+]ds* ]]; then
+	dfsgFilename="${dfsgFilename/.orig/$dfsgBits.orig}"
 fi
-tar -czf "$dir/$dfsgfilename" *
+tar -czf "$dir/$dfsgFilename" *
 cd "$dir"
 rm -rf "$tempdir"
-echo "Done pruning upstream tarball into $dfsgfilename"
 
+echo "Done pruning upstream tarball into $dfsgFilename"
 exit 0
